3873d498ba43f8e4b7fc36c6f8caf39e84a3d264
Kamie Short <kamie.short@gmail.com>
2022-06-23 09:14:20 -0700

    setup test, app files

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,22 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+const mockUser = {
+  firstName: 'firstNameTest',
+  lastName: 'lastNameTest',
+  email: 'test@user.com',
+  password: '12345',
+};
+
+const signUpAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? mockUser.password;
+
+  const agent = request.agent(app);
+
+const user = await UserService.create({...mockUser, ...userProps});
+
+};
+
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
